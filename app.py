5abe07b771f8ef97dfd4b978e092cbf4366ca020
Shagun Bhardwaj <b.shagun18@gmail.com>
2022-05-14 10:11:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@
 from flask import Flask, render_template, request, Markup
 import numpy as np
 import pandas as pd
-from utils.disease import disease_dic
-from utils.fertilizer import fertilizer_dic
+#from utils.disease import disease_dic
+#from utils.fertilizer import fertilizer_dic
 import requests
-import config
+#import config
 #import pickle
 import io
 import torch
